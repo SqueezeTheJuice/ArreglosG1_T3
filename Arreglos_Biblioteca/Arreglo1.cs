176d525e63485b55720bf03d32cccb2e83c0798b
SqueezeTheJuice <25030272@itesa.edu.mx>
2026-09-24 13:58:57 -0600

    Version_03:Metodo ordenar Ascendente y desendente

---
+++ Arreglos_Biblioteca/Arreglo1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arreglos_Biblioteca
+{
+    internal class Arreglo1
+    {
+
+
+
+        public void Ordenar()
+        {
+            Ordenar(true);
+        }
+
+
+        public void Ordenar(bool ascendente)
+        {
+            for (int i = 0; i < _tope - 1; i++)
+            {
+                for (int j = i +1;j < _tope; j++)
+                {
+                    if (ascendente)
+                    {
+
+                    }
+                    if (_arreglo[i] > _arreglo[j])
+                        {
+                         Cambiar(_arreglo[i], _arreglo[j]);
+                        }
+                    
+                }
+            }
+        }
+
+
+        public void Cambiar(int a, int b)
+        {
+            int aux = a;
+            a = b;
+            b = aux;
+        }
+    }
+}
